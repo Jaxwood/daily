608c956b26b84e662780be926bf367473216eebd
Jacob Lorenzen <jacob@lorenzen.me>
2020-05-12 13:53:07 +0200

    Clean up test names

--- tests/day001_test.py
+++ tests/day001_test.py
@@ -3,7 +3,7 @@ from problems.day001 import hasSumOfPair
 
 class TestDay001(unittest.TestCase):
 
-    def test_hasSumOf_whenSumIsTrue_ForPairOfNumbers(self):
+    def test_hasSumOfPair_whenSumIsFound_returnTrue(self):
         self.assertTrue(hasSumOfPair([10, 15, 3, 7], 17))
-    def test_hasSumOf_whenSumIsFalse(self):
+    def test_hasSumOfPair_whenSumNotFound_returnFalse(self):
         self.assertFalse(hasSumOfPair([10, 15, 3, 7], 19))
