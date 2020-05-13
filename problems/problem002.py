2fdf6f1de313ece469c3ecd752a4cc433a3a5ae0
Jacob Lorenzen <jacob@lorenzen.me>
2020-05-13 13:43:51 +0200

    Solution without division

--- problems/problem002.py
+++ problems/problem002.py
@@ -1,4 +1,5 @@
 from functools import reduce
+from math import exp, log
 
 def products(arr):
   """Given an array of integers, return a new array
@@ -9,4 +10,15 @@ def products(arr):
   res = reduce((lambda a, n: a * n), arr, 1)
   for a in arr:
     result.append(res / a)
+  return result
+
+def products2(arr):
+  """same problem as above with additional contraint that you cannot
+  use division"""
+  result = []
+  res = reduce((lambda a, n: a * n), arr, 1)
+  for a in arr:
+    # a / b = c
+    # exp(log(a) - log(b)) = c
+    result.append(round(exp(log(res) - log(a))))
   return result
