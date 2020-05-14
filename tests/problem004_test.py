e19a5daafba56377f4bf50799ac8eb137605e940
Jacob Lorenzen <jacob@lorenzen.me>
2020-05-14 20:54:03 +0200

    Add setup for problem 4

---
+++ tests/problem004_test.py
@@ -0,0 +1,11 @@
+import unittest
+from problems.problem004 import missing
+
+
+class TestProblem003(unittest.TestCase):
+
+    def test_missing_withoutDuplicates_returnsCorrectValue(self):
+        self.assertEqual(missing([3, 4, -1, 1]),  2)
+
+    def test_missing_withDuplicates_returnsCorrectValue(self):
+        self.assertEqual(missing([1, 2, 2, 0]),  3)
