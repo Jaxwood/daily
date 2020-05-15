69ffbdad9938257ea7987f5d50b73dd18fe7f4dd
Jacob Lorenzen <jacob@lorenzen.me>
2020-05-15 18:43:51 +0200

    Add better solution for problem 4

--- problems/problem004.py
+++ problems/problem004.py
@@ -3,8 +3,21 @@ def missing(input):
     integer in linear time and constant space. In other words,
     find the lowest positive integer that does not exist in the array.
     The array can contain duplicates and negative numbers as well."""
-    a = set(input)
+    a = set(input) # space is O(N) and not constant as the problem states :<
     for i in range(len(input)):
         if not (i+1) in a:
             return i + 1
+    return len(input) + 1
+
+def missing_optimized(input):
+    """same as above - but honor the constant space requirement"""
+    for i in range(len(input)):
+        cnt = i
+        while cnt < len(input):
+            if (input[cnt] == i + 1):
+                input[i], input[cnt] = input[cnt], input[i]
+                break
+            cnt += 1
+            if (cnt >= len(input)):
+                return i + 1
     return len(input) + 1
--- tests/problem004_test.py
+++ tests/problem004_test.py
@@ -1,14 +1,14 @@
 import unittest
-from problems.problem004 import missing
+from problems.problem004 import missing_optimized
 
 
 class TestProblem003(unittest.TestCase):
 
     def test_missing_withoutDuplicates_returnsCorrectValue(self):
-        self.assertEqual(missing([3, 4, -1, 1]),  2)
+        self.assertEqual(missing_optimized([3, 4, -1, 1]),  2)
 
     def test_missing_withDuplicates_returnsCorrectValue(self):
-        self.assertEqual(missing([1, 2, 2, 0]),  3)
+        self.assertEqual(missing_optimized([1, 2, 2, 0]),  3)
 
     def test_missing_fullRange_returnsCorrectValue(self):
-        self.assertEqual(missing([1, 2, 3]),  4)
+        self.assertEqual(missing_optimized([1, 2, 3]),  4)
