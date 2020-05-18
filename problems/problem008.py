c5c297a018f41ef3a0c2ed019ab793b5651bd79a
Jacob Lorenzen <jacob@lorenzen.me>
2020-05-18 18:03:13 +0200

    Solution for problem 8

--- problems/problem008.py
+++ problems/problem008.py
@@ -4,7 +4,15 @@ class Node:
         self.left = left
         self.right = right
 
+def equal(node):
+    if node.left and node.right:
+        return node.left.val == node.right.val == node.val
+    return False
 
-def unival(tree):
+def unival(tree, cnt=0):
     """Given the root to a binary tree, count the number of unival subtrees"""
-    return 0
+    # is it a leaf?
+    if tree.right == tree.left == None:
+        return cnt + 1
+    # sum rest of the tree
+    return unival(tree.left, cnt + 1 if equal(tree) else cnt) + unival(tree.right, cnt)
