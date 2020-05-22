30217f5ea7500cd17ebeccba1128ccfc5b95b4e1
Jacob Lorenzen <jacob@lorenzen.me>
2020-05-22 11:14:44 +0200

    Add problem statement

--- problems/problem010.py
+++ problems/problem010.py
@@ -1,6 +1,7 @@
 import asyncio
 
 async def schedule(delay, fn):
-    """text"""
+    """Implement a job scheduler which takes in a function f and an integer
+    n, and calls f after n milliseconds"""
     await asyncio.sleep(delay)
     return fn()
