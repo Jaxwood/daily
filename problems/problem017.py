bb0e57834822de3c183dd84a91525a55df59f1cd
Jacob Lorenzen <jacob@lorenzen.me>
2020-05-29 09:05:17 +0200

    Add logic for visiting the nodes of the tree

--- problems/problem017.py
+++ problems/problem017.py
@@ -1,13 +1,43 @@
-import re
-def longest_path(s: str, separator: str, total = 0) -> str:
-  if s.find(separator) == -1:
-    return s
+class Node:
+  def __init__(self, val, children = []):
+    self.children = children
+    self.val = val
 
-  for i in s.split(separator):
-    if i.find('\t') == -1:
-      print('match', i)
-    else:
-      return longest_path(i, separator + '\t')
+def parse(s: str) -> Node:
+  return Node("dir", [Node("subdir1", [Node("file1.ext"), Node("tsubsubdir1")]), Node("subdir2", [Node("subsubdir2", [Node("file2.ext")])])])
+
+def longest_path(ns: Node) -> int:
+  s = len(ns.val)
+  queue = []
+  best = 0
+
+  # initialize the queue
+  for i in ns.children:
+    queue.append((i,s))
+
+  # visit all nodes
+  while len(queue) > 0:
+    (q, t) = queue.pop(0)
+    # add 1 for the directory separator, e.g. 'dir/subdir1'
+    t += len(q.val) + 1
+    best = max(t, best)
+    for i in q.children:
+      queue.append((i, t))
+  
+  return best
 
 
 # "dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext"
+
+# "dir
+#   subdir1\n\t\tfile1.ext\n\t\tsubsubdir1
+#   subdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext"
+
+# "dir
+#   subdir1
+#     file1.ext
+#     tsubsubdir1
+#   subdir2
+#     subsubdir2\n\t\t\tfile2.ext"
+
+#  Node(dir, [Node(subdir1, [Node(file1.ext), Node(tsubsubdir1)]), Node(subdir2, [Node(subsubdir2, [Node(file2.ext")])])])]
--- tests/problem017_test.py
+++ tests/problem017_test.py
@@ -1,8 +1,9 @@
 import unittest
-from problems.problem017 import longest_path
+from problems.problem017 import parse, longest_path
 
 
 class TestProblem017(unittest.TestCase):
 
     def test_stream(self):
-        self.assertEqual(longest_path("dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext", '\n\t'), 32)
+        s = "dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext"
+        self.assertEqual(longest_path(parse(s)), 32)
