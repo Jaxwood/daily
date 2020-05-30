8e8791866eaacf62770639e5f5ec3801c4725a7e
Jacob Lorenzen <jacob@lorenzen.me>
2020-05-30 19:40:54 +0200

    Parse the string

--- problems/problem017.py
+++ problems/problem017.py
@@ -1,43 +1,56 @@
+import re
+
+
 class Node:
-  def __init__(self, val, children = []):
-    self.children = children
-    self.val = val
+    def __init__(self, val, children=[]):
+        self.children = children
+        self.val = val
 
-def parse(s: str) -> Node:
-  return Node("dir", [Node("subdir1", [Node("file1.ext"), Node("tsubsubdir1")]), Node("subdir2", [Node("subsubdir2", [Node("file2.ext")])])])
-
-def longest_path(ns: Node) -> int:
-  s = len(ns.val)
-  queue = []
-  best = 0
-
-  # initialize the queue
-  for i in ns.children:
-    queue.append((i,s))
-
-  # visit all nodes
-  while len(queue) > 0:
-    (q, t) = queue.pop(0)
-    # add 1 for the directory separator, e.g. 'dir/subdir1'
-    t += len(q.val) + 1
-    best = max(t, best)
-    for i in q.children:
-      queue.append((i, t))
-  
-  return best
+    def len(self) -> int:
+        return len(self.val)
 
 
-# "dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext"
+def parse(s: str, separator: str = '\\n\\t') -> Node:
+    res = re.split(separator+'(?!\\t)', s)
+    children = []
+    for i in res:
+        if has_subdirectory(i):
+            children.append(parse(i, separator + '\\t'))
+        elif is_file(i):
+            children.append(Node(i))
+    return Node(res[0], children)
 
-# "dir
-#   subdir1\n\t\tfile1.ext\n\t\tsubsubdir1
-#   subdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext"
 
-# "dir
-#   subdir1
-#     file1.ext
-#     tsubsubdir1
-#   subdir2
-#     subsubdir2\n\t\t\tfile2.ext"
+def has_subdirectory(s: str) -> bool:
+    return s.find('\n\t') != -1
 
-#  Node(dir, [Node(subdir1, [Node(file1.ext), Node(tsubsubdir1)]), Node(subdir2, [Node(subsubdir2, [Node(file2.ext")])])])]
+
+def is_file(s: str) -> bool:
+    return s.find('.') != -1
+
+
+def longest_path(n: Node) -> int:
+    s = n.len()
+    queue = []
+    best = 0
+
+    # initialize the queue
+    for i in n.children:
+        queue.append((i, s))
+
+    # visit all nodes
+    while len(queue) > 0:
+        (q, t) = queue.pop(0)
+        # add 1 for the directory separator, e.g. 'dir/subdir1'
+        t += q.len() + 1
+        best = max(t, best)
+        for i in q.children:
+            queue.append((i, t))
+
+    return best
+
+
+if __name__ == "__main__":
+    s = parse(
+        "dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext")
+    longest_path(s)
--- tests/problem017_test.py
+++ tests/problem017_test.py
@@ -5,5 +5,5 @@ from problems.problem017 import parse, longest_path
 class TestProblem017(unittest.TestCase):
 
     def test_stream(self):
-        s = "dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext"
-        self.assertEqual(longest_path(parse(s)), 32)
+        s = parse("dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext")
+        self.assertEqual(longest_path(s), 32)
