ee483b137283bb820a7e10318f82e4cfba6571bb
Jacob Lorenzen <jacob@lorenzen.me>
2020-06-07 12:30:53 +0200

    Update solution for problem 25

--- problems/problem025.py
+++ problems/problem025.py
@@ -1,2 +1,28 @@
-def regex(s: str, r: str) -> bool:
-    return s == r
+def matches_first_char(s, r):
+    return s[0] == r[0] or (r[0] == '.' and len(s) > 0)
+
+def matches(s, r):
+    """implement a function that takes in a string and a valid regular
+    expression and returns whether or not the string matches the regular
+    expression."""
+    if r == '':
+        return s == ''
+
+    if len(r) == 1 or r[1] != '*':
+        # The first character in the regex is not proceeded by a *.
+        if matches_first_char(s, r):
+            return matches(s[1:], r[1:])
+        else:
+            return False
+    else:
+        # The first character is proceeded by a *.
+        # First, try zero length.
+        if matches(s, r[2:]):
+            return True
+        # If that doesn't match straight away, then try globbing more prefixes
+        # until the first character of the string doesn't match anymore.
+        i = 0
+        while matches_first_char(s[i:], r):
+            if matches(s[i+1:], r[2:]):
+                return True
+            i += 1
--- tests/problem025_test.py
+++ tests/problem025_test.py
@@ -1,11 +1,11 @@
 import unittest
-from problems.problem025 import regex
+from problems.problem025 import matches
 
 
 class TestProblem025(unittest.TestCase):
 
     def test_regex(self):
-        self.assertEqual(regex('ray', 'ra.'), True)
-        self.assertEqual(regex('raymond', 'ra.'), False)
-        self.assertEqual(regex('chat', '.*at'), True)
-        self.assertEqual(regex('chats', 'ra.'), False)
+        self.assertEqual(matches('ray', 'ra.'), True)
+        self.assertEqual(matches('raymond', 'ra.'), False)
+        self.assertEqual(matches('chat', '.*at'), True)
+        self.assertEqual(matches('chats', 'ra.'), False)
