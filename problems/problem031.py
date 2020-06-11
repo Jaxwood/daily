0e927c7daa267d72af1f92dee2173ce8c417a14b
Jacob Lorenzen <jacob@lorenzen.me>
2020-06-11 20:33:18 +0200

    Add better implementation

--- problems/problem031.py
+++ problems/problem031.py
@@ -1,7 +1,23 @@
-def edit_distance(a: str, b: str) -> int:
+def edit_distance(s1: str, s2: str) -> int:
     """Given two strings, compute the edit distance between them."""
-    cnt = 0
-    for i in range(min(len(a), len(b))):
-        if a[i] != b[i]:
-            cnt += 1
-    return cnt + max(len(a), len(b)) - min(len(a), len(b))
+    x = len(s1) + 1
+    y = len(s2) + 1
+
+    A = [[-1 for i in range(x)] for j in range(y)]
+    for i in range(x):
+        A[0][i] = i
+
+    for j in range(y):
+        A[j][0] = j
+
+    for i in range(1, y):
+        for j in range(1, x):
+            if s1[j- 1] == s2[i - 1]:
+                A[i][j] = A[i - 1][j - 1]
+            else:
+                A[i][j] = min(
+                        A[i - 1][j] + 1,
+                        A[i][j - 1] + 1,
+                        A[i - 1][j - 1] + 1
+                        )
+    return A[y - 1][x - 1]
