efed186bd4c59fd6df3512165cf4fc01c3c00ffe
Jacob Lorenzen <jacob@lorenzen.me>
2020-06-15 20:13:33 +0200

    Naive impl

--- problems/problem034.py
+++ problems/problem034.py
@@ -3,4 +3,25 @@ def palindrome(s: str) -> str:
     fewest number of characters as possible anywhere in the word. If there is
     more than one palindrome of minimum length that can be made, return the
     lexicographically earliest one (the first one alphabetically)."""
-    return ""
+    result = s
+    if len(set(s)) == len(s):
+        e = len(s) - 1
+        prefix = ""
+        while not is_palindrome(prefix + result):
+            prefix += s[e]
+            print(prefix+result)
+            e -= 1
+        result = prefix + result
+
+    return result
+
+def is_palindrome(s: str) -> bool:
+    i = 0
+    j = len(s) - 1
+    while i < j:
+        if s[i] == s[j]:
+            i += 1
+            j -= 1
+        else:
+            return False
+    return True
--- tests/problem034_test.py
+++ tests/problem034_test.py
@@ -1,8 +1,12 @@
 import unittest
-from problems.problem034 import palindrome
+from problems.problem034 import palindrome, is_palindrome
 
 
 class TestProblem034(unittest.TestCase):
+
+    def test_is_palindrome(self):
+        expected = "ecarace"
+        self.assertEqual(True, is_palindrome(expected))
 
     def test_palindrome(self):
         expected = "ecarace"
